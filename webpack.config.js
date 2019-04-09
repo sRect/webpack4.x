8c4ca45017d4c464d9e2b6bed81f15c5b187c673
srect <fcqun@sina.cn>
2019-04-10 00:17:12 +0800

    :sparkles: feat: 多入口和多出口

--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,40 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: {
-    main: './src/index.js'
+  // entry: './src/index.js',
+  // entry: ['./src/index.js', './src/a.js'], // 将两个文件打包成一个
+  entry: { // 多入口
+    index: './src/index.js',
+    a: './src/a.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[hash:20].js'
+    filename: '[name].[hash:20].js' // 多出口
   },
   module: {},
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
+      filename: 'index.html',
       title: 'hello world',
       hash: true,
       minify: {
         collapseWhitespace: true, // 折叠空行
         removeAttributeQuotes: true
       },
-      template: './index.html'
+      template: './index.html',
+      chunks: ['index'] // index.html 引入index.js
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'a.html',
+      title: 'hello world',
+      hash: true,
+      minify: {
+        collapseWhitespace: true, // 折叠空行
+        removeAttributeQuotes: true
+      },
+      template: './index.html',
+      chunks: ['a'] // a.html引入a.js
     })
   ],
   devServer: {
