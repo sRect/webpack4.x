04633e6663659996baf2ebbe1c3dda27806f9ccd
Fang Chaoqun <fcqun@sina.cn>
2019-04-15 11:36:08 +0800

    :sparkles: feat: 增加对babel的支持

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test:/\.js$/,
+        use:{
+          loader:'babel-loader',
+        },
+        exclude:/node_modules/
+      },
+      {
         test: /\.css$/,
         // use: ['style-loader', 'css-loader'] // 从右往左
         use: ExtractTextWebpackPlugin.extract({
