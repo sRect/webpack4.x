12c6e3ed65baac09a92efb5777d2aa2be076152d
Fang Chaoqun <fcqun@sina.cn>
2019-04-15 11:14:00 +0800

    :sparkles: feat: 混淆压缩js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const glob = require('glob');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 打包html
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
-const PurifycssPlugin = require('purifycss-webpack');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin'); // 分离css
+const PurifycssPlugin = require('purifycss-webpack'); // 消除无用的css
+const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin'); // 混淆压缩js
+// const WebpackParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
 
 module.exports = {
+  // devtool: 'inline-source-map',
   // entry: './src/index.js',
   entry: ['./src/index.js', './src/a.js'], // 将两个文件打包成一个
   // entry: { // 多入口
@@ -43,11 +46,33 @@ module.exports = {
       }
     ]
   },
+  optimization: {
+    // webpack4中废弃了webpack.optimize.CommonsChunkPlugin插件,用新的配置项替代,把多次import的文件打包成一个单独的common.js
+    splitChunks: {
+      cacheGroups: {
+        commons: {  // 抽离自己写的公共代码
+          chunks: "initial",
+          name: "common", // 打包后的文件名，任意命名
+          minChunks: 2,//最小引用2次
+          minSize: 0 // 只要超出0字节就生成一个新包
+        },
+        vendor: {  // 抽离第三方插件
+          test: /node_modules/, // 指定是node_modules下的第三方包
+          chunks: 'initial',
+          minChunks: 2,
+          maxInitialRequests: 5,
+          minSize: 2,
+          name: 'vendor', // 打包后的文件名，任意命名
+          priority: 10 // 设置优先级，防止和自定义的公共代码提取时被覆盖，不进行打包
+        }
+      }
+    },
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new ExtractTextWebpackPlugin({
       filename: 'css/index.css',
-      disable: process.env.NODE_ENV === 'development' ? true : false // 是否禁用
+      disable: process.env.NODE_ENV === 'development' ? true : false // 是否禁用(development下禁用，因为要热更新)
     }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
@@ -76,6 +101,22 @@ module.exports = {
       paths: glob.sync(path.join(__dirname, 'src/*.html'))
     }),
     new webpack.HotModuleReplacementPlugin(),
+    new UglifyjsWebpackPlugin({
+      exclude: /\/node_modules/,
+      parallel: true,
+      sourceMap: true,
+      uglifyOptions: {
+        warnings: false,
+        parse: {},
+        compress: {},
+        mangle: true, // Note `mangle.properties` is `false` by default.
+        output: null,
+        toplevel: false,
+        nameCache: null,
+        ie8: false,
+        keep_fnames: false,
+      },
+    })
   ],
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
